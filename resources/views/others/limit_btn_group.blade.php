528dd502b2360b6f3ac7739c9d3c24f45973b4d2
AmilaKMAC <achathuranga452@gmail.com>
2026-03-06 10:09:46 +0530

    updated button group blade file

--- resources/views/others/limit_btn_group.blade.php
+++ resources/views/others/limit_btn_group.blade.php
@@ -1,4 +1,4 @@
-<div class="d-flex justify-content-between align-items-center px-3 py-2 bg-light border-top">
+<div class="d-flex justify-content-between align-items-center px-3  bg-light ">
     <div class="btn-group" role="group">
         <button type="button" class="btn btn-outline-secondary btn-sm limit-btn active" data-limit="5">5</button>
         <button type="button" class="btn btn-outline-secondary btn-sm limit-btn" data-limit="10">10</button>
